a7b28166b72d9fd12ee81cfc5d5d1ce1b091d8f1
Monty Zukowski <monty.zukowski@temboo.com>
2014-11-01 16:39:18 -0700

    alpha suffixes, option to only put in a year directory

--- rename.py
+++ rename.py
@@ -1,22 +1,62 @@
 import os
 import exifread
-import glob
 
-files = glob.glob("/Users/montyzukowski/personal/2014 Family Photos/*.jpg")
+badones = []
 
-for fn in files:
-    # Open image file for reading (binary mode)
-    f = open(fn, 'rb')
+#originals = '/Volumes/Backup/Pictures'
+#destination = "/Volumes/Backup/pix"
 
-    # Return Exif tags
-    tags = exifread.process_file(f)
-    if tags.has_key("EXIF DateTimeOriginal"):
-        dt = tags["EXIF DateTimeOriginal"].printable
-        date, time = dt.split()
-        year, month, day = date.split(":")
-        i=1
-        dest = os.path.join(os.path.dirname(fn), '%s-%s-%02d.jpg' % (month, day, i))
-        while os.path.exists(dest):
-            i=i+1
-            dest = os.path.join(os.path.dirname(fn), '%s-%s-%02d.jpg' % (month, day, i))
-        os.rename(fn, dest)
+originals = '/Users/montyzukowski/personal/2014 Family Photos'
+destination = "/Users/montyzukowski/personal/pix"
+
+useMonthDayDir = False
+
+def generate_suffix(i):
+    a = ord("a")
+    z = ord("z")
+    base = z-a+1
+    lo = i % base
+    hi = i / base
+    return chr(a + hi) + chr(a + lo)
+
+
+for root, dirs, files in os.walk(originals):
+    for name in files:
+        if not (name.endswith(".jpg") or name.endswith(".jpeg")
+                or name.endswith(".JPG") or name.endswith(".JPEG")):
+            continue
+        fn = os.path.join(root, name)
+        # Open image file for reading (binary mode)
+        f = open(fn, 'rb')
+
+        # Return Exif tags
+        try:
+            tags = exifread.process_file(f)
+            if tags.has_key("EXIF DateTimeOriginal"):
+                dt = tags["EXIF DateTimeOriginal"].printable
+                date, time = dt.split()
+                year, month, day = date.split(":")
+                i=0
+                destdir = os.path.join(destination, year)
+                if not os.path.exists(destdir):
+                    os.mkdir(destdir)
+                if useMonthDayDir:
+                    destdir = os.path.join(destdir, '%s-%s' % (month, day))
+                    if not os.path.exists(destdir):
+                        os.mkdir(destdir)
+                dest = os.path.join(destdir, '%s-%s-%s.jpg' % (month, day, generate_suffix(i)))
+                while os.path.exists(dest):
+                    i=i+1
+                    dest = os.path.join(destdir, '%s-%s-%s.jpg' % (month, day, generate_suffix(i)))
+                os.rename(fn, dest)
+                print 'saved', dest
+            else:
+                print 'no exif info for', fn
+                badones.append(fn)
+        except Exception, e:
+            badones.append(fn)
+            print e
+        f.close()
+
+for fn in badones:
+    print "couldn't deal with", fn
