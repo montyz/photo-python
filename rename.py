c3298e1acae8243304b101b3acb9164c0eacb844
Monty Zukowski <monty.zukowski@temboo.com>
2014-12-28 08:51:50 -0800

    different suffix

--- rename.py
+++ rename.py
@@ -1,18 +1,18 @@
 import os
 import exifread
+import string
 
 badones = []
 
-#originals = '/Volumes/Backup/Pictures'
+# originals = '/Volumes/Backup/Pictures'
 #destination = "/Volumes/Backup/pix"
 
 #originals = '/Users/montyzukowski/personal/pix'
 #destination = '/Users/montyzukowski/Dropbox/FamilyShared/Photos/2014 Family Photos'
 
-
-originals = '/Users/montyzukowski/Google Drive/Family Photos/archives'
+originals = '/Users/montyzukowski/Google Drive/Photo Archives/origs/Pictures/2001/01/01'
 #originals = '/Users/montyzukowski/personal/pix'
-destination = '/Users/montyzukowski/Google Drive/Family Photos/archives'
+destination = '/Users/montyzukowski/Google Drive/Photo Archives/'
 
 if not os.path.exists(destination):
     os.mkdir(destination)
@@ -20,13 +20,21 @@ if not os.path.exists(destination):
 useMonthDayDir = False
 useMonthDir = True
 
-def generate_suffix(i):
-    a = ord("a")
-    z = ord("z")
-    base = z-a+1
-    lo = i % base
-    hi = i / base
-    return chr(a + hi) + chr(a + lo)
+ltrs = string.ascii_letters + string.digits
+
+
+def generate_suffix(time, i=0):
+    hours, mins, secs = time.split(":")
+    h = int(hours)
+    m = int(mins)
+    s = int(secs) + i
+    if s >= len(ltrs):
+        s -= len(ltrs)
+        m += 1
+    if m >= len(ltrs):
+        m -= len(ltrs)
+        h += len(ltrs)
+    return ltrs[h] + ltrs[m] + ltrs[s]
 
 dt_fn_map = {}
 
@@ -46,11 +54,11 @@ def movePicture(dt, fn, destination):
         destdir = os.path.join(destdir, '%s' % (month))
         if not os.path.exists(destdir):
             os.mkdir(destdir)
-    dest = os.path.join(destdir, '%s-%s-%s-%s.jpg' % (year, month, day, generate_suffix(i)))
+    dest = os.path.join(destdir, '%s-%s-%s-%s.jpg' % (year, month, day, generate_suffix(time)))
     while os.path.exists(dest):
         i = i + 1
-        dest = os.path.join(destdir, '%s-%s-%s-%s.jpg' % (year, month, day, generate_suffix(i)))
-    # os.rename(fn, dest)
+        dest = os.path.join(destdir, '%s-%s-%s-%s.jpg' % (year, month, day, generate_suffix(time, i)))
+    #os.rename(fn, dest)
     print 'saved', dest
 
 
@@ -60,7 +68,11 @@ for root, dirs, files in os.walk(originals):
         if not (name.endswith(".jpg") or name.endswith(".jpeg")
                 or name.endswith(".JPG") or name.endswith(".JPEG")):
             continue
+        if 'Thumbs' in root:
+            print 'Skipping', root
+            continue
         fn = os.path.join(root, name)
+
         # Open image file for reading (binary mode)
         f = open(fn, 'rb')
 
@@ -82,10 +94,7 @@ keys = dt_fn_map.keys()
 print 'sorting', len(keys), 'keys'
 keys.sort()
 for key in keys:
-    try:
-        movePicture(key, dt_fn_map[key], destination)
-    except:
-        print 'bad key', key, 'for', dt_fn_map[key]
+    movePicture(key, dt_fn_map[key], destination)
 
 for fn in badones:
     print "couldn't deal with", fn
